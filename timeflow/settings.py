408de2b316f39a015a6eacfd02d88e30ec41aad8
Ismaelg12 <ismaelguitars2@gmail.com>
2026-08-14 10:48:01 -0300

    Banco

--- timeflow/settings.py
+++ timeflow/settings.py
@@ -121,6 +121,7 @@ WSGI_APPLICATION = 'timeflow.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -143,7 +144,7 @@ DATABASES = {
     }
 }
 
-"""
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
